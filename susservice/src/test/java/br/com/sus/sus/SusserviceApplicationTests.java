22bd828c34c6c9837c83a6a96c22cef7b56f8047
Olciandro <olciandro.magalhaes@basis.com.br>
2019-02-15 11:10:24 -0200

    novos projetos

---
+++ susservice/src/test/java/br/com/sus/sus/SusserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.sus.sus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SusserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
