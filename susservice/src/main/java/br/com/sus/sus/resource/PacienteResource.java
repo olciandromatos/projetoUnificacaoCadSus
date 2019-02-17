8574abea9942438078f321bb73d238f056f8165f
Olciandro <olciandro.magalhaes@basis.com.br>
2019-02-17 16:51:09 -0300

    Pacotes renomeados para mapeamento

--- susservice/src/main/java/br/com/sus/sus/resource/PacienteResource.java
+++ susservice/src/main/java/br/com/sus/sus/resource/PacienteResource.java
@@ -1,18 +1,35 @@
-package br.com.sus.resource;
+package br.com.sus.sus.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sus.sus.domain.Paciente;
+import br.com.sus.sus.service.PacienteService;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class PacienteResource {
 	
+	@Autowired
+	private PacienteService pacienteService;
+	
+	
 	@GetMapping
 	public String teste() {
 		return "testando...";
 	}
+	
+	@GetMapping("/paciente")
+	public List<Paciente> listar(){
+		return pacienteService.listarTodos();
+	}
+	
+	
 
 }
--- susservice/src/main/java/br/com/sus/sus/service/PacienteService.java
+++ susservice/src/main/java/br/com/sus/sus/service/PacienteService.java
@@ -1,4 +1,4 @@
-package br.com.sus.service;
+package br.com.sus.sus.service;
 
 import java.util.List;
 import java.util.Optional;
@@ -6,18 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.sus.domain.Paciente;
-import br.com.sus.repository.PacienteRepository;
+import br.com.sus.sus.domain.Paciente;
+import br.com.sus.sus.repository.PacienteRepository;
 
 @Service
 public class PacienteService {
 	
-	@Autowired
 	private PacienteRepository pacienteRepository;
-	
-	public PacienteService(PacienteRepository pacienteRepository) {
-		this.pacienteRepository = pacienteRepository;
-	}
 	
 	public Paciente cadastrar(Paciente paciente) {
 		return pacienteRepository.save(paciente);
@@ -31,5 +26,14 @@ public class PacienteService {
 	public List<Paciente> listarTodos(){
 		return pacienteRepository.findAll();
 	}
-
+	
+	public PacienteRepository getPacienteRepository() {
+		return pacienteRepository;
+	}
+	
+	@Autowired
+	public void setPacienteRepository(PacienteRepository pacienteRepository) {
+		this.pacienteRepository = pacienteRepository;
+	}
+	
 }
