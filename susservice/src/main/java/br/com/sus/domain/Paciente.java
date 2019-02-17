29da83ccb9eb98484550a62356c58cfa0795113e
Olciandro <olciandro.magalhaes@basis.com.br>
2019-02-17 00:14:59 -0300

    Criar entidade paciente e configurações

--- susservice/src/main/java/br/com/sus/domain/Paciente.java
+++ susservice/src/main/java/br/com/sus/domain/Paciente.java
@@ -1,5 +1,73 @@
 package br.com.sus.domain;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Paciente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@Column(name="ID_PACIENTE")
+	private Long id;
+	private String primeiroNome;
+	private String Sobrenome;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	private String telefone;
+	private LocalDate dataNascimento;
 
 }
--- susservice/src/main/java/br/com/sus/service/PacienteService.java
+++ susservice/src/main/java/br/com/sus/service/PacienteService.java
@@ -1,5 +1,35 @@
 package br.com.sus.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.sus.domain.Paciente;
+import br.com.sus.repository.PacienteRepository;
+
+@Service
 public class PacienteService {
+	
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
+	public PacienteService(PacienteRepository pacienteRepository) {
+		this.pacienteRepository = pacienteRepository;
+	}
+	
+	public Paciente cadastrar(Paciente paciente) {
+		return pacienteRepository.save(paciente);
+	}
+	
+	public Optional<Paciente> buscarPorId(Long id) {
+		Optional<Paciente> objeto = pacienteRepository.findById(id);
+		return objeto;
+	}
+	
+	public List<Paciente> listarTodos(){
+		return pacienteRepository.findAll();
+	}
 
 }
