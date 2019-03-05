12bca5e30dc7a4165e24fa166b9dea35a1d525b3
Olciandro <olciandro.magalhaes@basis.com.br>
2019-03-04 21:50:44 -0300

    insert pais, estado e municipio e ajustes

--- susservice/src/main/java/br/com/sus/sus/domain/Paciente.java
+++ susservice/src/main/java/br/com/sus/sus/domain/Paciente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Paciente implements Serializable{
