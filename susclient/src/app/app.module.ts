3da26e0bc773ac0e187dec6f5c622e2f67b30e12
olciandro.magalhaes <olciandro.magalhaes@basis.com.br>
2019-06-21 16:36:05 -0300

    Tela de cadastrar angular criada - code-02

--- susclient/src/app/app.module.ts
+++ susclient/src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonModule } from 'primeng/components/button/button';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { PanelModule } from 'primeng/panel';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { MenubarModule } from 'primeng/components/menubar/menubar';
+
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { PacienteComponent } from './paciente/paciente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    PacienteComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     DataTableModule,
     ButtonModule,
-    TooltipModule
+    TooltipModule,
+    PanelModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    InputTextModule,
+    InputMaskModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
