92ff48c96e7bab34747a6c0dd01ad2ca88279fda
olciandro.magalhaes <olciandro.magalhaes@basis.com.br>
2019-06-24 15:54:25 -0300

    adição do cross-origin no backend e insert de medicos

--- susservice/src/main/java/br/com/sus/sus/resource/MedicoResource.java
+++ susservice/src/main/java/br/com/sus/sus/resource/MedicoResource.java
@@ -3,6 +3,7 @@ package br.com.sus.sus.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.sus.sus.domain.Medico;
 import br.com.sus.sus.service.MedicoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MedicoResource {
