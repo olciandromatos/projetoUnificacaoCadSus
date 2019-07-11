902fd378a70c4c9d3d22af69f7dec4924e63f4b1
olciandro.magalhaes <olciandro.magalhaes@basis.com.br>
2019-07-11 10:10:56 -0300

    Adição de novas alterações

--- susclient/src/app/consulta/consulta.component.ts
+++ susclient/src/app/consulta/consulta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ComumServiceService } from '../comum-service.service';
+import { Paciente } from '../domain/Paciente';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-consulta',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaComponent implements OnInit {
 
-  constructor() { }
+  pacientes: Paciente[] = new Array;
+  paciente: Paciente = new Paciente;
+
+  constructor(private service: ComumServiceService) { }
 
   ngOnInit() {
+    this.getPaciente();
+  }
+
+  getPaciente() {
+    this.service.getPaciente().subscribe( result => this.pacientes = result);
+  }
+
+  imprimirPDF(){
+    console.log('imprimindo');
+    const doc = new jsPDF();
+    doc.text('Atestado Médico', 80, 15);
+    doc.setFont('Courier');
+    doc.setFontStyle('bold');
+    doc.setFontSize(10);
+    doc.setFillColor(50,50,50);
+    doc.text('Atesto que o sr.(a) '+ this.paciente.nome+' foi atendido em nosso consultório pela manhã', 10, 50);
+    doc.save('Atestado');
+    // doc.output("dataurlnewwindow");
   }
 
 }
--- susservice/src/main/java/br/com/sus/sus/domain/UnidadeSaude.java
+++ susservice/src/main/java/br/com/sus/sus/domain/UnidadeSaude.java
@@ -1,7 +1,5 @@
 package br.com.sus.sus.domain;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
