e71c1b640aea14772012fb892d9a5e38752c2f1d
olciandro <olciandro.m.matos@gmail.com>
2019-08-01 18:08:47 -0300

    Cadastro na tela de consulta pronta

--- susclient/src/app/consulta/consulta.component.ts
+++ susclient/src/app/consulta/consulta.component.ts
@@ -1,3 +1,4 @@
+import { Consulta } from './../domain/Consulta';
 import { Component, OnInit } from '@angular/core';
 import { ComumServiceService } from '../comum-service.service';
 import { Paciente } from '../domain/Paciente';
@@ -12,6 +13,7 @@ export class ConsultaComponent implements OnInit {
 
   pacientes: Paciente[] = new Array;
   paciente: Paciente = new Paciente;
+  consulta: Consulta = new Consulta;
 
   constructor(private service: ComumServiceService) { }
 
@@ -23,15 +25,21 @@ export class ConsultaComponent implements OnInit {
     this.service.getPaciente().subscribe( result => this.pacientes = result);
   }
 
-  imprimirPDF(){
+  cadastrar() {
+    this.service.cadastrarConsulta(this.consulta).subscribe(resultado => {
+      alert('Cadastrado com sucesso!');
+    });
+  }
+
+  imprimirPDF() {
     console.log('imprimindo');
     const doc = new jsPDF();
     doc.text('Atestado Médico', 80, 15);
     doc.setFont('Courier');
     doc.setFontStyle('bold');
     doc.setFontSize(10);
-    doc.setFillColor(50,50,50);
-    doc.text('Atesto que o sr.(a) '+ this.paciente.nome+' foi atendido em nosso consultório pela manhã', 10, 50);
+    doc.setFillColor(50, 50, 50);
+    doc.text('Atesto que o sr.(a) ' + this.paciente.nome + ' foi atendido em nosso consultório pela manhã', 10, 50);
     doc.save('Atestado');
     // doc.output("dataurlnewwindow");
   }
--- susservice/src/main/java/br/com/sus/sus/resource/ConsultaResource.java
+++ susservice/src/main/java/br/com/sus/sus/resource/ConsultaResource.java
@@ -3,8 +3,12 @@ package br.com.sus.sus.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +26,12 @@ public class ConsultaResource {
 	@GetMapping("/consulta")
 	public List<Consulta> consultas(){
 		return service.allConsultas();
+	}
+	
+	@PostMapping("/consulta")
+	public ResponseEntity<Consulta> salvar(@RequestBody Consulta consulta) {
+		service.cadastrar(consulta);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 }
